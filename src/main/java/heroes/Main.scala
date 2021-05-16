3a19e63e6d157cb4be5748fb9463533f8a24d3cf
nastiazaya <nastiazaytzev@gmail.com>
2021-05-16 16:51:59 +0300

    Add hero game and quoter lab

---
+++ src/main/java/heroes/Main.scala
@@ -0,0 +1,12 @@
+package heroes
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val hero1 = HeroFactory.getRandomHero
+    val hero2 = HeroFactory.getRandomHero
+    println(hero1)
+    println(hero2)
+    GameManager.fight(hero1,hero2)
+
+  }
+}
